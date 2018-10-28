038c60147a7aacba9f48f0c426a035c964cefe63
shubdut87 <shubdut87@gmail.com>
2018-10-29 00:49:01 +0530

    Commit10282018 - web server

--- weather-app/app.js
+++ weather-app/app.js
@@ -21,10 +21,16 @@ var encodedAddress = encodeURIComponent(argv.address);
 //console.log(encodedAddress);
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+  url: `https://mapsgoogleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+  if(error){
+    console.log('Unable to connect to Google servers.');
+  } else if(body.status === 'ZERO_RESULTS'){
+    console.log('Unable to find the address');
+  } else if(body.status === 'OK'){
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
 });
